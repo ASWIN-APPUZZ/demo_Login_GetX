13767246a9fe87df5d6846ca30e56c86e3c99a71
aswin <aswin0484@gmail.com>
2024-01-11 11:59:13 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/demo_login_get/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_login_get
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:demo_login_get/screens/auth/auth_screen.dart';
+import 'package:demo_login_get/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: AuthScreen()
+    );
+  }
+}
